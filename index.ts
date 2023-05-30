150e9d62c964be31c2c65d713d0c25766935ac2f
Santosh Managuli <santoshm@trinesis.com>
2023-05-30 19:02:07 +0530

    added fuctions

--- index.ts
+++ index.ts
@@ -2,8 +2,21 @@ import { question } from "readline-sync";
 
 function main(): void{
     const firstStr: string = question('Enter the desired number:');
+    const operator: string = question('Enter operator:')
     const secondStr: string = question('Enter the second number:');
-    console.log(firstStr+secondStr);
+
+    const isValid: boolean = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+    console.log(isValid);
+}
+
+function isNumber(str: string): boolean{
+    const num: number = parseInt(str);
+    const isNum: boolean = !isNaN(num);
+    return isNum
+}
+
+function isOperator(op: string): boolean{
+    return op === '+' || op === '*' || op === '/' || op === '-'
 }
 
 main();
