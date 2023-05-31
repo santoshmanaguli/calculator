8318d2ff23278a5e6661cd279026c3df13a6934a
Santosh Managuli <santoshm@trinesis.com>
2023-05-31 11:45:02 +0530

    added calcualtion step

--- index.js
+++ index.js
@@ -3,7 +3,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var readline_sync_1 = require("readline-sync");
 function main() {
     var firstStr = (0, readline_sync_1.question)('Enter the desired number:');
+    var operator = (0, readline_sync_1.question)('Enter operator:');
     var secondStr = (0, readline_sync_1.question)('Enter the second number:');
-    console.log(firstStr + secondStr);
+    var isValid = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
+    if (isValid) {
+        var result = calculate(firstStr, operator, secondStr);
+        console.log(result);
+    }
+    else {
+        console.log('not valid input');
+        main();
+    }
+}
+function calculate(firstStr, operator, secondStr) {
+    var first = parseInt(firstStr);
+    var second = parseInt(secondStr);
+    switch (operator) {
+        case '+':
+            return first + second;
+        case '-':
+            return first - second;
+        case '/':
+            return first / second;
+        case '*':
+            return first * second;
+    }
+}
+function isNumber(str) {
+    var num = parseInt(str);
+    var isNum = !isNaN(num);
+    return isNum;
+}
+function isOperator(op) {
+    return op === '+' || op === '*' || op === '/' || op === '-';
 }
 main();
--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
 import { question } from "readline-sync";
 
+type Operator = '+' | '/' | '-' | '*'
+
 function main(): void{
     const firstStr: string = question('Enter the desired number:');
     const operator: string = question('Enter operator:')
     const secondStr: string = question('Enter the second number:');
 
     const isValid: boolean = isNumber(firstStr) && isOperator(operator) && isNumber(secondStr);
-    console.log(isValid);
+    if (isValid) {
+        const result = calculate(firstStr, operator as Operator, secondStr );
+        console.log(result);
+    } else {
+        console.log('not valid input');
+        main();
+    }
+}
+
+function calculate(firstStr: string, operator: Operator, secondStr: string){
+    const first  = parseInt(firstStr);
+    const second = parseInt(secondStr);
+    switch (operator) {
+        case '+':
+            return first + second
+        case '-':
+            return first - second
+        case '/':
+            return first / second
+        case '*':
+            return first * second
+    }
 }
 
 function isNumber(str: string): boolean{
